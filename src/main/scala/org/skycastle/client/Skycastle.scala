72bfb0f742ce771a3dca0c3e8261b9cd7a1aecb3
zzorn <zzorn@iki.fi>
2010-04-06 22:37:33 +0300

    Add stub for client entry point.

---
+++ src/main/scala/org/skycastle/client/Skycastle.scala
@@ -0,0 +1,14 @@
+package org.skycastle.client
+
+/**
+ * Main entry point for Skycastle client.
+ * 
+ * @author Hans Haggstrom
+ */
+object Skycastle {
+
+  def main(args: Array[String]) {
+    print( "Skycastle client starting." )
+  }
+}
+
