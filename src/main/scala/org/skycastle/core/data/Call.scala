a380e890633ad8348769187630fcffb2622638a6
zzorn <zzorn@iki.fi>
2010-06-06 20:27:28 +0300

    Some comments and added a stub for measures.

--- src/main/scala/org/skycastle/core/data/Call.scala
+++ src/main/scala/org/skycastle/core/data/Call.scala
@@ -5,6 +5,8 @@ import function.FunctionCall
 /**
  * A function call.
  */
+// TODO: A call to a list with an index returns the element at the index,
+// and a call to a map with a string returns the element with the name -> support those also.
 class Call extends AbstractValue {
 
   type Self = this.type
---
+++ src/main/scala/org/skycastle/core/data/Measure.scala
@@ -0,0 +1,10 @@
+package org.skycastle.core.data
+
+/**
+ * A numerical value with a unit.
+ * Can be converted to a number.
+ */
+// TODO: Support getting the measure and checking the type at the same time.
+class Measure {
+  
+}
