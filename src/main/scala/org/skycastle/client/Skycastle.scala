1b2a50ed237627bcec6678866991ddd1bacf32cc
zzorn <zzorn@iki.fi>
2011-03-09 17:16:44 +0200

    Added some test 3D scene.

--- src/main/scala/org/skycastle/client/Skycastle.scala
+++ src/main/scala/org/skycastle/client/Skycastle.scala
@@ -1,5 +1,10 @@
 package org.skycastle.client
 
+import com.jme3.app.SimpleApplication
+import com.jme3.scene.shape.Box
+import com.jme3.scene.Geometry
+import com.jme3.material.Material
+import com.jme3.math.{ColorRGBA, Vector3f}
 
 /**
  * Main entry point for Skycastle client.
@@ -13,25 +18,33 @@ TODO: Give options to
   - list the ones the player has an account on
   - get list of all running servers (through server to server networking)
  */
-object Skycastle {
+object Skycastle extends SimpleApplication {
 
   def main(args: Array[String]) {
     println( "Skycastle client starting." )
 
-    // Setup screen
-//    val screen = new Screen()
-//    screen.run()
-
-    // Get server based on user input (for now, just create a local one)
+    // Get server based on user input or create a local one (for now, just create a local one)
 
     // Allow user to select an avatar/game on the account, or create a new one
 
-    // Enter main game loop
-
+    // TODO:
     // draw scene
     // get any updates
     // listen to player inputs, map controls to actions, call server with action
 
+    start()
+  }
+
+  /** Setup 3D view */
+  override def simpleInitApp = {
+
+    // Spike 3D scene..
+    val b = new Box(Vector3f.ZERO, 1, 1, 1);
+    val geom = new Geometry("Box", b);
+    val mat = new Material(assetManager, "Common/MatDefs/Misc/SolidColor.j3md");
+    mat.setColor("m_Color", ColorRGBA.Red);
+    geom.setMaterial(mat);
+    rootNode.attachChild(geom);
   }
 }
 
